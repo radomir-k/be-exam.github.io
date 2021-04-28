524a496c86d335a0b44670c584ff5eefa493b5c9
radomir-k <radomir.kartas@gmail.com>
2021-04-28 19:18:02 +0300

    prerel

---
+++ js/tempFunction.js
@@ -0,0 +1,14 @@
+// window.onload = function() {
+// };
+
+// $( window ).load(function() {
+//   // Run code
+// });
+
+// function refresh() {
+//   window.location.reload();
+// }
+
+// $(document).ready(function() {
+//   createLocalJSON(queryStr);
+// });
